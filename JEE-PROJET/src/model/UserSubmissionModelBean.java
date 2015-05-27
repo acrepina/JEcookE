d1b7f8d794fc45a5ce9faedabe352e9244748587
Joris Bouquet <joris.bouquet@mastertp>
2015-05-27 16:31:21 +0200

    Creation page inscription

--- JEE-PROJET/src/model/UserSubmissionModelBean.java
+++ JEE-PROJET/src/model/UserSubmissionModelBean.java
@@ -4,7 +4,7 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
 @ManagedBean
-@RequestScoped //Durée de vue uniquement lors d'une requète
+@RequestScoped //Durée de vie uniquement lors d'une requète
 //même propriétés que UserModelBean mais portée différente
 
 public class UserSubmissionModelBean extends UserModelBean{
